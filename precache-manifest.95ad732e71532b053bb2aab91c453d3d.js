f1edb0c78e34d5990b12105b41868c7f7ef68bbf
Ori Naftaly <naftalyblues@gmail.com>
2020-06-29 11:44:38 -0500

    Updates

---
+++ precache-manifest.95ad732e71532b053bb2aab91c453d3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c3f12474ac23cbf16341285fff2340",
+    "url": "/Quizz-Chellenge/index.html"
+  },
+  {
+    "revision": "710cc9847f9af3e81bed",
+    "url": "/Quizz-Chellenge/static/css/main.f369ae53.chunk.css"
+  },
+  {
+    "revision": "693101803ae49bfb7bfb",
+    "url": "/Quizz-Chellenge/static/js/2.96e21b58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quizz-Chellenge/static/js/2.96e21b58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "710cc9847f9af3e81bed",
+    "url": "/Quizz-Chellenge/static/js/main.aea81ad4.chunk.js"
+  },
+  {
+    "revision": "efe31b5f3e59722e7134",
+    "url": "/Quizz-Chellenge/static/js/runtime-main.9ebfa927.js"
+  }
+]);
